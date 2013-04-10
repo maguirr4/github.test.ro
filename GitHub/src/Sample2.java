e1bea8e9cd9671171a10c8367a2ac7af601669ad
maguirr4 <maguirr4@gmail.com>
2013-04-10 10:48:18 +0100

    Added sample2 class with an add method

--- GitHub/src/Sample2.java
+++ GitHub/src/Sample2.java
@@ -1,13 +1,15 @@
-package ie.gcd.griffith;
 
-public class Sample {
+public class Sample2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		add(2,5);
+	}
+	public static int add(int x, int y){
+		return x+y;
 	}
 
 }
